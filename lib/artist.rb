9842f212177902a93e058fd2ab2e0ec21a25d760
norawolf <noraevogt@gmail.com>
2019-01-13 17:06:55 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   include Memorable::InstanceMethods
-  extend Findable
-  include Paramable
+  include Paramable::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
