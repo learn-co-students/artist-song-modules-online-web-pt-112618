6e12fea1934544e70004bf5bbdd9cae283d88c76
github username <github email address>
2019-01-18 10:47:53 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,22 @@
 require 'pry'
 
 class Song
+  extend Memorable::ClassMethods
+  extend Findable
+  include Paramable
+  include Memorable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
+  
   def self.all
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
-  def artist=(artist)
+  def artist=(artist) #wHAT IS THIS??? SETTER??? It calls an Artist object with the song associated to it??
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
